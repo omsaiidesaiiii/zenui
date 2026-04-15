44da8337880be5db5f1bf4c82ea2768dc3a64050
omsaidesaii <omsaidesai9@gmail.com>
2026-04-16 01:49:22 +0530

    port updated

--- ui-registry-api/index.js
+++ ui-registry-api/index.js
@@ -50,6 +50,7 @@ app.get("/components/:name", (req, res) => {
   res.json(data);
 });
 
-app.listen(3001, () => {
-  console.log("Server running on port 3001");
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
